97cce9465d25c6a3cc265b83197deacf79045c56
Bernst4315 <bernst4315@gmail.com>
2025-11-15 10:22:47 -0700

    added a counter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const addBtn = document.querySelector(".add");
+const homescoreEl = document.querySelector(".homescore")
+
+let count = 0;
+
+addBtn.addEventListener("click", add)
+
+function add(){
+    // console.log(`Start: ${count}`)
+    count++
+    // console.log(`End: ${count}`)
+    homescoreEl.innerHTML = count; 
+}
