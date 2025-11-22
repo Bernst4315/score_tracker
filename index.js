14e13bef8375991ae3796524a9338a83be66b669
Bernst4315 <bernst4315@gmail.com>
2025-11-22 14:04:50 -0700

    scores now increment separately

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const addBtn = document.querySelector(".add");
-const homeIncrementDiv = document.querySelector("#homeIncrement")
+// const addBtn = document.querySelector(".add");
+// const homeIncrementDiv = document.querySelector("#homeIncrement")
 const scoreboardDiv = document.querySelector(".scoreboard")
-const homescoreEl = document.querySelector(".homescore")
+// const homescoreEl = document.querySelector(`.homescore`)
 
-let count = 0;
+//let count = 0;
 
 //addBtn.addEventListener("click", add)
 //homeIncrementDiv.addEventListener("click", add)
@@ -11,11 +11,15 @@ scoreboardDiv.addEventListener("click", add)
 
 function add(e){
     // console.log(e.target.parentElement.id)
-    console.log(e.target)
+    console.log(e.target.attributes.for)
     // console.log(e.target.value)
 
     // console.log(`Start: ${count}`)
+    const score = document.querySelector(`.${e.target.attributes.for.value}`)
+    let count = Number(score.textContent)
     if(e.target.value)  count += Number(e.target.value)
     // console.log(`End: ${count}`)
-    homescoreEl.innerHTML = count; 
+    //homescoreEl.innerHTML = count
+    console.log(score)
+    score.innerHTML = count
 }
