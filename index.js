ac45a037b3489f307ee5d94ec252a54f16e5cfbf
Bernst4315 <bernst4315@gmail.com>
2025-11-22 14:26:02 -0700

    cleaned up unused code

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-// const addBtn = document.querySelector(".add");
-// const homeIncrementDiv = document.querySelector("#homeIncrement")
 const scoreboardDiv = document.querySelector(".scoreboard")
-// const homescoreEl = document.querySelector(`.homescore`)
 
-//let count = 0;
-
-//addBtn.addEventListener("click", add)
-//homeIncrementDiv.addEventListener("click", add)
 scoreboardDiv.addEventListener("click", add)
 
 function add(e){
-    // console.log(e.target.parentElement.id)
-    console.log(e.target.attributes.for)
-    // console.log(e.target.value)
 
-    // console.log(`Start: ${count}`)
-    const score = document.querySelector(`.${e.target.attributes.for.value}`)
+    const score = document.querySelector(`.${e.target.name}`)
     let count = Number(score.textContent)
     if(e.target.value)  count += Number(e.target.value)
-    // console.log(`End: ${count}`)
-    //homescoreEl.innerHTML = count
-    console.log(score)
     score.innerHTML = count
 }
