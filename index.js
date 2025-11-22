13425d053b3edb10bdd1ead594d01053e70e45e3
Bernst4315 <bernst4315@gmail.com>
2025-11-22 13:30:30 -0700

    home score increment buttons work

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const addBtn = document.querySelector(".add");
+const homeIncrementDiv = document.querySelector("#homeIncrement")
+const scoreboardDiv = document.querySelector(".scoreboard")
 const homescoreEl = document.querySelector(".homescore")
 
 let count = 0;
 
-addBtn.addEventListener("click", add)
+//addBtn.addEventListener("click", add)
+//homeIncrementDiv.addEventListener("click", add)
+scoreboardDiv.addEventListener("click", add)
 
-function add(){
+function add(e){
+    // console.log(e.target.parentElement.id)
+    console.log(e.target)
+    // console.log(e.target.value)
+
     // console.log(`Start: ${count}`)
-    count++
+    if(e.target.value)  count += Number(e.target.value)
     // console.log(`End: ${count}`)
     homescoreEl.innerHTML = count; 
 }
